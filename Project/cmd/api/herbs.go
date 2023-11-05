990b971e91d37c05db6125ad3cc78e3023a5b08e
Erasyl Tastybai <e_tastybai@kbtu.kz>
2023-11-05 18:11:55 +0600

    Chapter 7

--- Project/cmd/api/herbs.go
+++ Project/cmd/api/herbs.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"gourmetspices.yerassyl.net/internal/data"
 	"gourmetspices.yerassyl.net/internal/validator"
 	"net/http"
-	"time"
-
-	"gourmetspices.yerassyl.net/internal/data"
 )
 
 func (app *application) createHerbHandler(w http.ResponseWriter, r *http.Request) {
+
 	var input struct {
 		Name         string     `json:"name"`
 		Description  string     `json:"description"`
@@ -36,7 +36,20 @@ func (app *application) createHerbHandler(w http.ResponseWriter, r *http.Request
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
-	fmt.Fprintf(w, "%+v\n", input)
+
+	err = app.models.Herbs.Insert(herb)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/herbs/%d", herb.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"herb": herb}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 func (app *application) showHerbHandler(w http.ResponseWriter, r *http.Request) {
@@ -46,16 +59,99 @@ func (app *application) showHerbHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	herb := data.Herb{
-		ID:           id,
-		CreatedAt:    time.Time{},
-		Name:         "Acacia Powder",
-		Description:  "Acacia spp. is found across sub-Saharan Africa and is known for its gum resin. Acacia gum, or gum Arabic, is harvested, dried, then milled into a fine powder. Acacia powder is a popular addition to a multitude of products as it acts to bind formulas and stabilize emulsions.",
-		Price:        8.25,
-		CulinaryUses: []string{"emulsifier", "stabilizer", "thickener"},
-		Version:      1,
+	herb, err := app.models.Herbs.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
+
 	err = app.writeJSON(w, http.StatusOK, envelope{"herb": herb}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) updateHerbHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	herb, err := app.models.Herbs.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	var input struct {
+		Name         string     `json:"name"`
+		Description  string     `json:"description"`
+		Price        data.Price `json:"price"`
+		CulinaryUses []string   `json:"culinary_uses"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	herb.Name = input.Name
+	herb.Description = input.Description
+	herb.Price = input.Price
+	herb.CulinaryUses = input.CulinaryUses
+
+	v := validator.New()
+	if data.ValidateHerb(v, herb); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Herbs.Update(herb)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"herb": herb}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) deleteHerbHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Herbs.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "herb successfully deleted"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
--- Project/internal/data/herbs.go
+++ Project/internal/data/herbs.go
@@ -1,6 +1,9 @@
 package data
 
 import (
+	"database/sql"
+	"errors"
+	"github.com/lib/pq"
 	"time"
 
 	"gourmetspices.yerassyl.net/internal/validator"
@@ -31,4 +34,106 @@ func ValidateHerb(v *validator.Validator, herb *Herb) {
 	v.Check(len(herb.CulinaryUses) >= 1, "culinary_uses", "must contain at least 1 use")
 	v.Check(len(herb.CulinaryUses) <= 5, "culinary_uses", "must not contain more than 5 uses")
 	v.Check(validator.Unique(herb.CulinaryUses), "culinary_uses", "must not contain duplicate values")
+}
+
+type HerbModel struct {
+	DB *sql.DB
+}
+
+func (h HerbModel) Insert(herb *Herb) error {
+
+	query := `INSERT INTO herbs (name, description, price, culinary_uses) 
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	args := []interface{}{
+		herb.Name,
+		herb.Description,
+		herb.Price,
+		pq.Array(herb.CulinaryUses),
+	}
+
+	return h.DB.QueryRow(query, args...).Scan(
+		&herb.ID,
+		&herb.CreatedAt,
+		&herb.Version,
+	)
+}
+func (h HerbModel) Get(id int64) (*Herb, error) {
+
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `SELECT id, created_at, name, description, price, culinary_uses, version
+		FROM herbs
+		WHERE id = $1`
+
+	var herb Herb
+
+	err := h.DB.QueryRow(query, id).Scan(
+		&herb.ID,
+		&herb.CreatedAt,
+		&herb.Name,
+		&herb.Description,
+		&herb.Price,
+		pq.Array(&herb.CulinaryUses),
+		&herb.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+
+	}
+
+	return &herb, nil
+}
+
+func (h HerbModel) Update(herb *Herb) error {
+
+	query := `UPDATE herbs
+		SET name = $1, description = $2, price = $3, culinary_uses = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+
+	args := []interface{}{
+		herb.Name,
+		herb.Description,
+		herb.Price,
+		pq.Array(herb.CulinaryUses),
+		herb.ID,
+	}
+
+	return h.DB.QueryRow(query, args...).Scan(&herb.Version)
+}
+
+func (h HerbModel) Delete(id int64) error {
+
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM herbs
+		WHERE id = $1`
+
+	result, err := h.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
+	return nil
 }
