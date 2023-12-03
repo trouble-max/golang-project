7fad629444c28445d78e69ba340e5fafdbe0bf4e
Erasyl Tastybai <e_tastybai@kbtu.kz>
2023-12-03 16:16:37 +0600

    Chapter 14

--- Project/cmd/api/main.go
+++ Project/cmd/api/main.go
@@ -5,10 +5,12 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"gourmetspices.yerassyl.net/internal/data"
 	"gourmetspices.yerassyl.net/internal/jsonlog"
+	"gourmetspices.yerassyl.net/internal/mailer"
 
 	_ "github.com/lib/pq"
 )
@@ -29,12 +31,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -53,6 +64,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "ed45646c3f8e5b", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "273d9cdfa9d147", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "GourmetSpices <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
@@ -69,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
